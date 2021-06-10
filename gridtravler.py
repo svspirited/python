6d9aad56c237ff7c4cca8af5fb08f0437a8b2338
Rob Place <rwplace@gmail.com>
2021-06-10 09:19:42 -0400

    initial version

---
+++ gridtravler.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+# from functools import lru_cache
+
+# @lru_cache
+def gridPaths(m, n, memo={}):
+    
+    if m == 1 and n == 1:
+        return 1
+    if m == 0 or n == 0:
+        return 0
+
+    key = (m,n)
+    if key in memo:
+        return memo[key]
+    
+    memo[key] = gridPaths(m - 1, n, memo) + gridPaths(m, n -1, memo)
+    return memo[key]
+    
+def main():
+    print(gridPaths(1,1))
+    print(gridPaths(0,1))
+    print(gridPaths(18,18))
+    print(gridPaths(100,100))
+    
+    pass
+
+if __name__ == "__main__":
+    main()
+    
