8a25116a22fa4b7f1513d01a3d3aa191cde7e780
Rob Place <rwplace@gmail.com>
2021-06-14 16:54:26 -0400

    updated to determine when no possible combination returns the required result

--- bestSum.py
+++ bestSum.py
@@ -1,6 +1,5 @@
 import argparse
 import copy
-import numpy as np
 
 def bestSum(m, n, combination=[], memo={}):
     if m == 0:
@@ -14,16 +13,22 @@ def bestSum(m, n, combination=[], memo={}):
     
     for num in n:
         remainder = m - num
-        remainderCombination = bestSum(remainder, n, combination)
+        remainderCombination = bestSum(remainder, n, combination, memo)
 
         if remainderCombination != None:
             combination = copy.deepcopy(remainderCombination)
             combination.append(num)
+            
             if len(shortestCombination) == 0 or len(combination) < len(shortestCombination):
                 shortestCombination = combination
                 memo[m] = shortestCombination
+                combination = None
 
-    return shortestCombination
+    if sum(shortestCombination) == m:
+        memo[m] = shortestCombination        
+        return memo[m]
+
+    return None
     
 def main():
     parser = argparse.ArgumentParser()
@@ -33,8 +38,8 @@ def main():
     args = parser.parse_args()
 
     m = args.number
-    n = list(args.numbers)
-
+    n = args.numbers
+    
     n.sort(reverse=True) # Try the largest numbers first to avoid max recursion error
     sums = bestSum(m, n)
 
