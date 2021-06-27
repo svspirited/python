a4d5771332b2e2ef32046223d2882ebeacf1f25f
Rob Place <rwplace@gmail.com>
2021-06-27 11:53:35 -0400

    initial version

---
+++ dynamic-programming/fib-tab.py
@@ -0,0 +1,24 @@
+def fib(num):
+    nums = []
+    for n in range(0, num+1):
+        nums.append(0)
+
+    nums[1] = 1
+    
+    for n in range(len(nums)):
+        if n <= (num-2):
+            nums[n+1] = nums[n+1] + nums[n]
+            nums[n+2] = nums[n+2] + nums[n]
+        elif n <= (num-1):
+            nums[n+1] = nums[n+1] + nums[n]
+
+    return(nums[n])
+
+
+def main():
+    num = fib(50)
+    print(num)
+
+if __name__ == "__main__":
+    main()
+        
